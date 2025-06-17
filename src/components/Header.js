367b2938c3cd12581b539a5f477ddeffb7662368
neelesh18904 <neelesh18903@gmail.com>
2025-06-17 13:40:57 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto Hunter
+              CryptoPulse
             </Typography>
             {/* <Button color="inherit">Login</Button> */}
             <Select
